6285c501de3f3064264afada13b059aafd3f9887
Pedro <Pedro@DESKTOP-NUMEN>
2024-04-14 10:43:14 -0300

    Add project files.

---
+++ Osculari/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdint>
+#include <cassert>
+
+uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a = 255)
+{
+	return (a << 24) + (b << 16) + (g << 8) + r;
+}
+
+unpack_color
+
+int main()
+{
+	const uint8_t r = 255;
+	const uint8_t g = 255;
+	const uint8_t b = 255;
+
+
+	return 0;
+}
